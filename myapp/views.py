2ab6d9c1745d2ba09304a1021cd8dc6e08d7cbea
Rahul Yadav <89911219+rahuly29@users.noreply.github.com>
2025-06-13 15:16:27 +0530

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import BookForm
+
+# Create
+def book_create(request):
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'book_form.html', {'form': form})
+
+# Read
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+# Update
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'book_form.html', {'form': form})
+
+# Delete
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book_delete.html', {'book': book})
