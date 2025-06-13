10eb1d03917bfcbdf2b2b633b2ad06b0acdd025b
Rahul Yadav <89911219+rahuly29@users.noreply.github.com>
2025-06-13 16:37:39 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    publish_date = models.DateField()
+    publish_date = models.DateField(input_formats=['%d%m%Y'],
+        widget=models.DateInput(format='%d%m%Y', attrs={'placeholder': 'DDMMYYYY'}))
 
     def __str__(self):
         return self.title
